1397370341420003791782d991f0abda4a532c56
jhkang1313 <jhkang1313@gmail.com>
2025-03-05 00:49:17 +0900

    Running Spring Batch

---
+++ src/test/java/com/facamp/hellospringbatch/HelloSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facamp.hellospringbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
