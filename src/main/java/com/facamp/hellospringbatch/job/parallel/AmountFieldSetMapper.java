3aa890ccfa42ca7a2cbc677e82be53fcc7d63248
jhkang1313 <jhkang1313@gmail.com>
2025-03-10 00:40:47 +0900

    MultiThreadStep 으로 구현

---
+++ src/main/java/com/facamp/hellospringbatch/job/parallel/AmountFieldSetMapper.java
@@ -0,0 +1,19 @@
+package com.facamp.hellospringbatch.job.parallel;
+
+import com.facamp.hellospringbatch.dto.AmountDto;
+import org.springframework.batch.item.file.mapping.FieldSetMapper;
+import org.springframework.batch.item.file.transform.FieldSet;
+import org.springframework.validation.BindException;
+
+public class AmountFieldSetMapper implements FieldSetMapper<AmountDto> {
+
+
+  @Override
+  public AmountDto mapFieldSet(FieldSet fieldSet) throws BindException {
+    AmountDto amountDto = new AmountDto();
+    amountDto.setIndex(fieldSet.readInt(0));
+    amountDto.setName(fieldSet.readString(1));
+    amountDto.setAmount(fieldSet.readInt(2));
+    return amountDto;
+  }
+}
